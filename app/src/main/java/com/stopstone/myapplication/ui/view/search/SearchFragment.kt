1207baacf82e33f5f798b31608498ce88a62517d
stopstone <wltjr6432@naver.com>
2024-07-06 06:05:00 +0900

    feat: 키보드 검색버튼 클릭 시 검색 활성화

--- app/src/main/java/com/stopstone/myapplication/ui/view/search/SearchFragment.kt
+++ app/src/main/java/com/stopstone/myapplication/ui/view/search/SearchFragment.kt
@@ -1,10 +1,11 @@
 package com.stopstone.myapplication.ui.view.search
 
-import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -35,9 +36,8 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.rvSearchTrackList.adapter = adapter
-        setSearchButton()
-        setListeners()
         observeTracks()
+        setListeners()
     }
 
     override fun onDestroyView() {
@@ -45,22 +45,13 @@ class SearchFragment : Fragment() {
         _binding = null
     }
 
-    private fun setSearchButton() {
-        binding.btnSearchTrack.setOnClickListener {
-            val track = binding.etSearchTrack.text.toString()
-            when (track.isNotEmpty()) {
-                true -> viewModel.searchTracks(track)
-                false -> showToastMessage(getString(R.string.search_empty_message))
-            }
-            hideKeyboard()
+    private fun searchTracks() {
+        val track = binding.etSearchTrack.text.toString()
+        when (track.isNotEmpty()) {
+            true -> viewModel.searchTracks(track)
+            false -> showToastMessage(getString(R.string.search_empty_message))
         }
-    }
-
-    private fun setListeners() {
-        adapter.setOnItemClickListener { track ->
-            val action = SearchFragmentDirections.actionSearchToTrackConfirmDialog(track)
-            findNavController().navigate(action)
-        }
+        hideKeyboard()
     }
 
     private fun observeTracks() {
@@ -74,12 +65,33 @@ class SearchFragment : Fragment() {
         }
     }
 
+    private fun setListeners() {
+        adapter.setOnItemClickListener { track ->
+            val action = SearchFragmentDirections.actionSearchToTrackConfirmDialog(track)
+            findNavController().navigate(action)
+        }
+
+        binding.btnSearchTrack.setOnClickListener {
+            searchTracks()
+        }
+
+        binding.etSearchTrack.setOnEditorActionListener { v, actionId, event ->
+            when (actionId) {
+                EditorInfo.IME_ACTION_SEARCH -> {
+                    searchTracks()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
     private fun showToastMessage(message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun hideKeyboard() {
-        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val imm = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(requireView().windowToken, 0)
     }
 }
