cd5063d4f0495b03e2a0076c7d23d64a39632d41
StopStone <wltjr6432@naver.com>
2024-08-16 04:02:48 +0900

    상세 페이지 코드 정리 및 update시 comment 업데이트 제거

--- app/src/main/java/com/stopstone/myapplication/ui/detail/TrackDetailActivity.kt
+++ app/src/main/java/com/stopstone/myapplication/ui/detail/TrackDetailActivity.kt
@@ -3,7 +3,9 @@ package com.stopstone.myapplication.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.stopstone.myapplication.databinding.ActivityTrackDetailBinding
 import com.stopstone.myapplication.ui.detail.viewmodel.TrackDetailViewModel
@@ -23,40 +25,46 @@ class TrackDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setLayout()
+        setListeners()
+        collectViewModel()
+    }
 
-        val date = with(args.DailyTrack) {
-            val day = args.DailyTrack.id
-            val year = args.DailyTrack.year
-            val month = args.DailyTrack.month
-            convertDayToDate(year, month, day)
-        }
+    private fun setLayout() {
+        with(args.DailyTrack) {
+            convertDayToDate(year, month, id) // 저장된 트랙의 날짜를, date 타입의 시간으로 변경
+        }.also { viewModel.setCurrentDate(it) }
 
         with(binding) {
             val track = args.DailyTrack.track!!
             ivTrackDetailAlbumCover.loadImage(track.imageUrl)
             tvTrackDetailTitle.text = track.title
             tvTrackDetailArtist.text = track.artist
-
-            btnTrackDetailSave.setOnClickListener {
-                viewModel.updateComment(etTrackDetailComment.text.toString())
-            }
         }
+    }
 
-        viewModel.setCurrentDate(date)
+    private fun collectViewModel() = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            launch { collectComment() }
+        }
+    }
 
-        lifecycleScope.launch {
-            viewModel.comment.collectLatest { comment ->
-                if (comment != binding.etTrackDetailComment.text.toString()) {
-                    binding.etTrackDetailComment.setText(comment)
-                    binding.etTrackDetailComment.setSelection(comment.length)
-                }
-            }
+    private suspend fun collectComment() {
+        viewModel.comment.collectLatest { comment ->
+            binding.etTrackDetailComment.setText(comment)
+            binding.etTrackDetailComment.setSelection(comment.length)
+        }
+    }
+
+    private fun setListeners() {
+        binding.btnTrackDetailSave.setOnClickListener {
+            viewModel.updateComment(binding.etTrackDetailComment.text.toString())
         }
     }
 
     private fun convertDayToDate(year: Int, month: Int, day: Int): Date {
         return Calendar.getInstance().apply {
-            set(year, month, day, 0, 0, 0)
+            set(year, month - 1, day, 0, 0, 0)
             set(Calendar.MILLISECOND, 0)
         }.time
     }
