017cdf5eb581d15fa33864d687c5cb35e0cd2aca
stopstone <wltjr6432@naver.com>
2025-10-22 16:45:23 +0900

    refactor: RecommendRepository 관련 파일 삭제

--- app/src/main/java/com/stopstone/myapplication/di/RepositoryModule.kt
+++ app/src/main/java/com/stopstone/myapplication/di/RepositoryModule.kt
@@ -1,19 +1,18 @@
 package com.stopstone.myapplication.di
 
 import com.stopstone.myapplication.data.repository.common.AuthRepositoryImpl
-import com.stopstone.myapplication.data.repository.home.CalendarRepositoryImpl
 import com.stopstone.myapplication.data.repository.common.PlayListRepositoryImpl
+import com.stopstone.myapplication.data.repository.common.TrackRepositoryImpl
+import com.stopstone.myapplication.data.repository.home.CalendarRepositoryImpl
 import com.stopstone.myapplication.data.repository.search.SearchHistoryRepositoryImpl
 import com.stopstone.myapplication.data.repository.search.SearchRepositoryImpl
-import com.stopstone.myapplication.data.repository.common.TrackRepositoryImpl
-import com.stopstone.myapplication.data.repository.home.RecommendRepositoryImpl
 import com.stopstone.myapplication.domain.repository.common.AuthRepository
-import com.stopstone.myapplication.domain.repository.home.CalendarRepository
 import com.stopstone.myapplication.domain.repository.common.PlayListRepository
+import com.stopstone.myapplication.domain.repository.common.TrackRepository
+import com.stopstone.myapplication.domain.repository.home.CalendarRepository
+import com.stopstone.myapplication.domain.repository.home.RecommendRepository
 import com.stopstone.myapplication.domain.repository.search.SearchHistoryRepository
 import com.stopstone.myapplication.domain.repository.search.SearchRepository
-import com.stopstone.myapplication.domain.repository.common.TrackRepository
-import com.stopstone.myapplication.domain.repository.home.RecommendRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -23,46 +22,27 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindTrackRepository(repository: TrackRepositoryImpl): TrackRepository
 
     @Binds
     @Singleton
-    abstract fun bindTrackRepository(
-        repository: TrackRepositoryImpl
-    ): TrackRepository
+    abstract fun bindCalendarRepository(repository: CalendarRepositoryImpl): CalendarRepository
 
     @Binds
     @Singleton
-    abstract fun bindCalendarRepository(
-        repository: CalendarRepositoryImpl
-    ): CalendarRepository
+    abstract fun bindSearchRepository(repository: SearchRepositoryImpl): SearchRepository
 
     @Binds
     @Singleton
-    abstract fun bindSearchRepository(
-        repository: SearchRepositoryImpl
-    ): SearchRepository
+    abstract fun bindPlayListRepository(repository: PlayListRepositoryImpl): PlayListRepository
 
     @Binds
     @Singleton
-    abstract fun bindPlayListRepository(
-        repository: PlayListRepositoryImpl
-    ): PlayListRepository
+    abstract fun bindAuthRepository(repository: AuthRepositoryImpl): AuthRepository
 
     @Binds
     @Singleton
-    abstract fun bindAuthRepository(
-        repository: AuthRepositoryImpl
-    ): AuthRepository
-
-    @Binds
-    @Singleton
-    abstract fun bindSearchHistoryRepository(
-        repository: SearchHistoryRepositoryImpl
-    ): SearchHistoryRepository
-
-    @Binds
-    @Singleton
-    abstract fun bindRecommendRepository(
-        repository: RecommendRepositoryImpl
-    ): RecommendRepository
+    abstract fun bindSearchHistoryRepository(repository: SearchHistoryRepositoryImpl): SearchHistoryRepository
 }
