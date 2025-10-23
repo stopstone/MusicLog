7a70f0949641360b1f4a157f7dc9c54bd9a255d5
StopStone <wltjr6432@naver.com>
2025-10-23 20:21:17 +0900

    refactor: 사용하지 않는 데이터 클래스 삭제

--- app/src/main/java/com/stopstone/musicplaylist/data/model/entity/AudioFeatures.kt
+++
@@ -1,8 +0,0 @@
-package com.stopstone.musicplaylist.data.model.entity
-
-data class AudioFeatures(
-    val danceability: Float,
-    val energy: Float,
-    val valence: Float,
-    val acousticness: Float
-)
