8db54f1b73ccee9c099d2bc4c3bc2513b4c447fe
StopStone <wltjr6432@naver.com>
2025-10-31 22:27:26 +0900

    build: 앱 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.stopstone.musicplaylist"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.3.4"
 
         buildConfigField("String", "CLIENT_ID", "\"${properties["client.id"]}\"")
