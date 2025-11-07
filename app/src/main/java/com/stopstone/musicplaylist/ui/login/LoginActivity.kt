c6c616a9c555c7472b7e3b73eb3a2a00b90d7562
stopstone <wltjr6432@naver.com>
2025-11-07 17:04:12 +0900

    refactor: MainViewModel 삭제 및 스플래시 화면 로직을 LoginActivity로 이동

--- app/src/main/java/com/stopstone/musicplaylist/ui/login/LoginActivity.kt
+++ app/src/main/java/com/stopstone/musicplaylist/ui/login/LoginActivity.kt
@@ -1,21 +1,42 @@
 package com.stopstone.musicplaylist.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.stopstone.musicplaylist.R
+import com.stopstone.musicplaylist.databinding.ActivityLoginBinding
+import com.stopstone.musicplaylist.ui.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        setupWindowInsets()
+    }
+
+    private fun setupWindowInsets() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    // 메인 화면으로 이동
+    private fun navigateToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
