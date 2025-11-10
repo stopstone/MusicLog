edecad4654a08e5e01f862cad4152bb1bd6238a5
stopstone <wltjr6432@naver.com>
2025-11-10 23:19:02 +0900

    feat: MY 화면 추가

---
+++ app/src/main/java/com/stopstone/musicplaylist/ui/my/MyFragment.kt
@@ -0,0 +1,41 @@
+package com.stopstone.musicplaylist.ui.my
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.stopstone.musicplaylist.databinding.FragmentMyBinding
+
+class MyFragment : Fragment() {
+    private var _binding: FragmentMyBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var context: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        this.context = context
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentMyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
