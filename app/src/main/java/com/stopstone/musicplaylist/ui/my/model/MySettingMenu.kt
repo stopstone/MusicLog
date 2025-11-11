659f7d0dcb176b71295caf9ef02773a00fad60ca
stopstone <wltjr6432@naver.com>
2025-11-11 17:17:41 +0900

    feat: 인스타 공유 설정 화면 이동 추가

--- app/src/main/java/com/stopstone/musicplaylist/ui/my/model/MySettingMenu.kt
+++ app/src/main/java/com/stopstone/musicplaylist/ui/my/model/MySettingMenu.kt
@@ -1,13 +1,14 @@
 package com.stopstone.musicplaylist.ui.my.model
 
 import com.stopstone.musicplaylist.R
+import com.stopstone.musicplaylist.ui.insta_share_setting.InstagramShareSettingActivity
 
 enum class MySettingMenu(
     val title: String,
-    val destinationId: Int,
+    val destinationId: Int = 0,
 ) {
     ACCOUNT("계정 설정", R.id.navigation_user_setting),
-    EMOTION_TAG("감정 태그 관리", 0), // TODO: R.id.emotionTagFragment로 변경
-    NOTIFICATION("알림 설정", 0), // TODO: R.id.notificationSettingFragment로 변경
-    INSTAGRAM_SHARE("인스타 공유 설정", 0), // TODO: R.id.instagramShareSettingFragment로 변경
+    EMOTION_TAG("감정 태그 관리"),
+    NOTIFICATION("알림 설정"),
+    INSTAGRAM_SHARE("인스타 공유 설정", R.id.navigation_insta_share_setting),
 }
