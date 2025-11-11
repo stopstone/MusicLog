6dc49c8dca2b87bdd47d5cb16a0b0c64587fb352
stopstone <wltjr6432@naver.com>
2025-11-12 00:20:09 +0900

    feat: 감정 태그 관리 화면 추가

--- app/src/main/java/com/stopstone/musicplaylist/ui/emotion_setting/EmotionSettingActivity.kt
+++ app/src/main/java/com/stopstone/musicplaylist/ui/emotion_setting/EmotionSettingActivity.kt
@@ -1,17 +1,25 @@
 package com.stopstone.musicplaylist.ui.emotion_setting
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.stopstone.musicplaylist.R
+import com.stopstone.musicplaylist.databinding.ActivityEmotionSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class EmotionSettingActivity : AppCompatActivity() {
+    private val binding: ActivityEmotionSettingBinding by lazy {
+        ActivityEmotionSettingBinding.inflate(LayoutInflater.from(this))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_emotion_setting)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/stopstone/musicplaylist/ui/my/model/MySettingMenu.kt
+++ app/src/main/java/com/stopstone/musicplaylist/ui/my/model/MySettingMenu.kt
@@ -8,7 +8,7 @@ enum class MySettingMenu(
     val destinationId: Int = 0,
 ) {
     ACCOUNT(R.string.label_my_menu_account_setting, R.id.navigation_user_setting),
-    EMOTION_TAG(R.string.label_my_menu_emotion_tag),
+    EMOTION_TAG(R.string.label_my_menu_emotion_tag, R.id.navigation_emotion_setting),
 
     //    NOTIFICATION(R.string.label_my_menu_notification),
     INSTAGRAM_SHARE(R.string.label_my_menu_instagram_share, R.id.navigation_insta_share_setting),
