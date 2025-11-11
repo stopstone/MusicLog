187e9568ecaaf2a1d1255435e01ce4db36e93f78
stopstone <wltjr6432@naver.com>
2025-11-12 00:16:03 +0900

    feat: EmotionSettingActivity 추가

---
+++ app/src/main/java/com/stopstone/musicplaylist/ui/emotion_setting/EmotionSettingActivity.kt
@@ -0,0 +1,21 @@
+package com.stopstone.musicplaylist.ui.emotion_setting
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.stopstone.musicplaylist.R
+
+class EmotionSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_emotion_setting)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
