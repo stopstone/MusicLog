2c42f40d89219fd552b29fbeb5d3b8f019da537a
stopstone <wltjr6432@naver.com>
2025-11-11 23:55:07 +0900

    chore: 알림 설정 주석

--- app/src/main/java/com/stopstone/musicplaylist/ui/my/model/MySettingMenu.kt
+++ app/src/main/java/com/stopstone/musicplaylist/ui/my/model/MySettingMenu.kt
@@ -9,6 +9,7 @@ enum class MySettingMenu(
 ) {
     ACCOUNT(R.string.label_my_menu_account_setting, R.id.navigation_user_setting),
     EMOTION_TAG(R.string.label_my_menu_emotion_tag),
-    NOTIFICATION(R.string.label_my_menu_notification),
+
+    //    NOTIFICATION(R.string.label_my_menu_notification),
     INSTAGRAM_SHARE(R.string.label_my_menu_instagram_share, R.id.navigation_insta_share_setting),
 }
