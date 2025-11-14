b91e7127358a038aa7618f843e9bcade47c8c708
stopstone <wltjr6432@naver.com>
2025-11-14 15:17:32 +0900

    feat: SignatureListActivity 추가 및 네비게이션 설정

---
+++ app/src/main/java/com/stopstone/musicplaylist/ui/signature_list/SignatureListActivity.kt
@@ -0,0 +1,28 @@
+package com.stopstone.musicplaylist.ui.signature_list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.stopstone.musicplaylist.R
+import com.stopstone.musicplaylist.databinding.ActivitySignatureListBinding
+
+class SignatureListActivity : AppCompatActivity() {
+    private val binding: ActivitySignatureListBinding by lazy {
+        ActivitySignatureListBinding.inflate(
+            layoutInflater,
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
